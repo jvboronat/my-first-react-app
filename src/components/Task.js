c18ae8c038c3b1aebd267cf7f71b4e74873847e8
Jose Vte. Boronat Domenech <jobodo@softin.com>
2021-02-01 20:39:07 +0100

    Tras instalar json-server, genero el archivo db.json y añado en el package.json la línea para poder arrancar el server con npm run server

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,11 +2,11 @@ import { FaTimes } from "react-icons/fa";
 
 function Task({ task, onDelete, toggleReminder }) {
   return (
-    <div className={`task ${task.reminder ? 'reminder':''}`} 
-    onDoubleClick={() => toggleReminder(task.id)}
+    <div className={`task ${task.reminder ? 'reminder' : ''}`}
+      onDoubleClick={() => toggleReminder(task.id)}
     >
       <h3>
-        {task.text} <FaTimes color="red"  onClick={() => onDelete(task.id)}/>
+        {task.text} <FaTimes color="red" onClick={() => onDelete(task.id)} />
       </h3>
       <p>{task.day}</p>
     </div>
